eec963e1227a7c695d30bf690673b2a913c367ab
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 13:56:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -46,9 +46,10 @@ def take_a_number(line, joiner)
 end
    
    def now_serving(katz_deli)
-     while katz_deli >= 0 
+     if katz_deli >= 0 
               puts "Currently serving #{katz_deli.first}."
         katz_deli.shift
+    else   
         puts "There is nobody waiting to be served!"
       
       end
