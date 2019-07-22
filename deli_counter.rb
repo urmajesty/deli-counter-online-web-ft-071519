04a0679239dfcde0c7d6bf7de38cf40bff98e277
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 14:02:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -48,6 +48,7 @@ end
    def now_serving(katz_deli)
      if katz_deli >= 0
        puts "Currently serving #{katz_deli.first}."
+     else
        puts "There is nobody waiting to be served!"
      
               
