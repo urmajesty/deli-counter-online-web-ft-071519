d0d58e716f98aedf0c0b29ef0a5796d3de81cb72
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 13:27:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -40,9 +40,9 @@ def line(array)
   end
   
 end
-def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+def take_a_number(line, joiner)
+  line.push(joiner) # could say: "line << new_person"
+  puts "Welcome, #{joiner}. You are number #{line.length} in line."
 end
    
    
